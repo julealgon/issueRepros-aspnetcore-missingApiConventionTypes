6cca9329f4bee3984e10a68080f65d5eb51ca014
Juliano Leal Goncalves <jgoncalves@affinitiv.com>
2025-05-15 18:13:29 -0300

    Add reproduction scenario for missing API-convention related types

---
+++ App/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+// All of these work just fine switching to 2.2.0 on the csproj
+_ = typeof(ProducesDefaultResponseTypeAttribute);
+_ = typeof(ApiConventionNameMatchAttribute);
+_ = typeof(ApiConventionNameMatchBehavior);
+_ = typeof(ApiConventionTypeMatchBehavior);
+_ = typeof(ApiConventionTypeAttribute);
